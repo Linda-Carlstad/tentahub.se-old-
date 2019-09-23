a815191cfd850cf8dc5066f5fb70401f89e061af
Felix Wetell <felix.wetell@gmail.com>
2019-09-23 20:40:35 +0200

    Authorization when changing the user model #4

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,7 +13,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Model' => 'App\Policies\ModelPolicy',
+        'App\Model' => 'App\Policies\ModelPolicy',
+        'App\User' => 'App\Policies\UserPolicy',
     ];
 
     /**
