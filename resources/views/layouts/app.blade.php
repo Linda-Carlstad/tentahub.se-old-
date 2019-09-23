d391ed83b4f3e1b7a219e4e9d81bfaa188854cfd
Felix Wetell <felix.wetell@gmail.com>
2019-09-23 19:49:03 +0200

    Added routes in navbar #2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,6 +50,8 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('dashboard') }}">Profil</a>
+                                    <a class="dropdown-item" href="{{ route('profile') }}">Inställningar</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
