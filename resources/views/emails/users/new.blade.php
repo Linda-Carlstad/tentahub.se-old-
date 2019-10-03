094f7a9bc4946908c867bfd635b18253a4b51e10
Felix Wetell <felix.wetell@gmail.com>
2019-10-03 15:00:40 +0200

    Mail for new user

---
+++ resources/views/emails/users/new.blade.php
@@ -0,0 +1,14 @@
+@extends( 'layouts.mail' )
+@section( 'title', 'Ny användare' )
+@section( 'content' )
+
+    <h3>Hejsan {{ $user->name }}</h3>
+    <p>
+        Den här e-postadressen har blivit registrerad på <a href="https://tentahub.se">https://tentahub.se</a>.
+        <br><br>
+        <b>E-postadress:</b> {{ $user->email }}
+        <br>
+        <b>Lösenord:</b> {{ $password }}
+    </p>
+
+@endsection
