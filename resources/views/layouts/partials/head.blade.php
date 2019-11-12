17577dfcc3a3925fe97a2b653f571346fb5fe945
Felix Wetell <felix.wetell@gmail.com>
2019-11-12 15:06:52 +0100

    Adds functionality to webpack

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -4,16 +4,18 @@
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<title>{{ config('app.name', 'Tentahub') }} - @yield( 'title')</title>
+<title>{{ config('app.name', 'Tentahub') }} - @yield( 'title', 'Studentens samlingsplats för tentor' )</title>
 
 <!-- Styles -->
-<link rel="dns-prefetch" href="//fonts.gstatic.com">
+<link rel="dns-prefetch" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" rel="stylesheet" type="text/css">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css" rel="stylesheet" type="text/css">
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 <!-- Scripts -->
 <script defer src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 <script defer src="{{ asset('js/app.js') }}"></script>
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
