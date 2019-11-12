b80f9e241bd2b317cb9b1d9a9405f3db4f5113ef
Felix Wetell <felix.wetell@gmail.com>
2019-11-12 14:18:58 +0100

    Abstracted flash messages #17

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,23 +35,7 @@
         @include( 'layouts.partials.navbar' )
 
         <main class="pb-4">
-            @if( session()->has( 'success' ) )
-                <div class="alert alert-success alert-dismissible fade show" role="alert">
-                    {{ session()->get( 'success' ) }}
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-            @endif
-
-            @if( session()->has( 'error' ) )
-                <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                    {{ session()->get( 'error' ) }}
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-            @endif
+            @include( 'layouts.partials.flash-messages' )
             @yield('content')
         </main>
     </div>
