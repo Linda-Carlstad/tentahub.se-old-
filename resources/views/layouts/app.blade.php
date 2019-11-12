5a3563876f342863767afd45f1a05a15348c7722
Felix Wetell <felix.wetell@gmail.com>
2019-11-12 14:20:28 +0100

    Abstracted head #17

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,34 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Tentahub') }} - @yield( 'title')</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" rel="stylesheet" type="text/css">
-
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-136489552-2"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){ dataLayer.push( arguments ); }
-        gtag( 'js', new Date() );
-
-        gtag( 'config', 'UA-136489552-2' );
-    </script>
+    @include( 'layouts.partials.head' )
 </head>
 <body>
     <div id="app">
@@ -41,26 +14,6 @@
         @include( 'layouts.partials.footer' )
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js" data-cfasync="false"></script>
-    <script>
-      window.addEventListener("load", function(){
-        window.cookieconsent.initialise({
-          "palette": {
-            "popup": {
-              "background": "#343a40"
-            },
-            "button": {
-              "background": "#660023",
-              "color": "#fff"
-            }
-          },
-          "content": {
-        		"message": "Den här webbplatsen använder kakor för att du ska få den bästa upplevelsen på vår hemsida.",
-        		"dismiss": "Uppfattat!",
-        		"link": "Läs mer"
-      	  }
-        })
-      });
-    </script>
+
 </body>
 </html>
---
+++ resources/views/layouts/partials/head.blade.php
@@ -0,0 +1,49 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<title>{{ config('app.name', 'Tentahub') }} - @yield( 'title')</title>
+
+<!-- Styles -->
+<link rel="dns-prefetch" href="//fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" rel="stylesheet" type="text/css">
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+<!-- Scripts -->
+<script defer src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script defer src="{{ asset('js/app.js') }}"></script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script defer src="https://www.googletagmanager.com/gtag/js?id=UA-136489552-2"></script>
+<script defer>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){ dataLayer.push( arguments ); }
+    gtag( 'js', new Date() );
+
+    gtag( 'config', 'UA-136489552-2' );
+</script>
+
+<script defer src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js" data-cfasync="false"></script>
+<script defer>
+    window.addEventListener("load", function(){
+        window.cookieconsent.initialise({
+            "palette": {
+                "popup": {
+                    "background": "#343a40"
+                },
+                "button": {
+                    "background": "#660023",
+                    "color": "#fff"
+                }
+            },
+            "content": {
+                "message": "Den här webbplatsen använder kakor för att du ska få den bästa upplevelsen på vår hemsida.",
+                "dismiss": "Uppfattat!",
+                "link": "Läs mer"
+            }
+        })
+    });
+</script>
