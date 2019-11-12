c4683d817a04491c7ad5162ba15940e2438a2725
Felix Wetell <felix.wetell@gmail.com>
2019-11-12 13:49:30 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 // Authentication Routes...
-Route::get('logga-in', 'Auth\LoginController@showLoginForm')->name('login');
+Route::get('logga-in', 'Auth\LoginController@showLoginForm')->name('login-form');
 Route::post('login', 'Auth\LoginController@login')->name( 'login' );
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
