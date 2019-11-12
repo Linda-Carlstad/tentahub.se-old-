1468e1508323185f08167ded0166aeb2d1f99a31
Felix Wetell <felix.wetell@gmail.com>
2019-11-12 23:50:13 +0100

    Updates email verify design #18

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,28 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
 
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-		                <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-	                </form>
-                </div>
+    <section class="section">
+        <div class="columns level">
+            <div class="column is-half is-widescreen level-item">
+                <h1 class="title">Verifiera din e-postadress här</h1>
+                <p>
+                    För att vi ska kunna veta att det är du, så behöver du verifiera din e-postadress!
+                    Kolla i din inkorg så kan vi vara säkra på att det är du.
+                    <br>
+                    <br>
+                    Fick du inget mail?
+                    <br>
+                    Klicka nedan för att skicka ett nytt verifieringsmail.
+                </p>
+                <br>
+                <form>
+                    @csrf
+                    <button type="submit" class="button is-primary">Skicka igen</button>
+                </form>
+            </div>
+            <div class="column is-half is-widescreen level-item has-text-centered">
+                <figure class="image">
+                    <img src="https://via.placeholder.com/400x400.png/09f/fff" />
+                </figure>
             </div>
         </div>
-    </div>
-</div>
+    </section>
+
 @endsection
