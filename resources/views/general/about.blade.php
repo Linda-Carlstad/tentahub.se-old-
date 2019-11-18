4b45531d1147c5103960854fe4f1012f8cb5ae9a
Felix Wetell <felix.wetell@gmail.com>
2019-11-18 11:29:54 +0100

    Creates about view #27

---
+++ resources/views/general/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section( 'title', 'Om oss' )
+@section('content')
+
+    <section class="about columns level">
+        <div class="column has-text-centered level-item">
+
+        </div>
+        <div class="column has-text-centered level-item">
+
+        </div>
+    </section>
+
+@endsection
