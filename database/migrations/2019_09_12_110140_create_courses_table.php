78af7c262b4875b6255fdefb3b510ce904e71f6a
Felix Wetell <felix.wetell@gmail.com>
2019-11-26 15:24:41 +0100

    Updated tables

--- database/migrations/2019_09_12_110140_create_courses_table.php
+++ database/migrations/2019_09_12_110140_create_courses_table.php
@@ -18,9 +18,9 @@ class CreateCoursesTable extends Migration
             $table->integer('association_id');
             $table->string('name');
             $table->string('code');
-            $table->longText( 'description' );
+            $table->longText( 'description' )->nullable();
             $table->decimal( 'points', 8, 1 );
-            $table->string( 'url' );
+            $table->string( 'url' )->nullable();
             $table->timestamps();
         });
     }
