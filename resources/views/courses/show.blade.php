7d6ad8c9a426bbacf9c014b1e87b2fa0fdd7b162
Felix Wetell <felix.wetell@gmail.com>
2019-12-02 18:13:25 +0100

    Displays exams in fancy list on courses #32

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -9,29 +9,27 @@
             <p>{{ $course->description }}</p>
             <a target="_blank" href="{{ $course->url }}">{{ $course->url }}</a>
             <hr>
-            @if( $exams->isNotEmpty() )
-                <h3 class="subtitle is-3">Tentor:</h3>
-                <ul>
+            @if( $exams->isEmpty() )
+                <p>Inga tentor att visa. Lägg till en!</p>
+                <br>
+            @else
+                <div class="list is-hoverable">
                     @foreach( $exams as $exam )
-                        <li>
+                        <p class="list-item">
                             <a href="{{ route( 'exams.show', $exam->id ) }}">
                                 {{ $exam->name }}
                             </a>
-                        </li>
+                            -
+                            <a href="{{ route( 'exams.download', $exam->id ) }}">
+                                Ladda ner
+                            </a>
+                        </p>
                     @endforeach
-                </ul>
-                <br>
-                @if( Auth::user()->role === 'super' || Auth::user()->role === 'admin' && Auth::user()->association->university->id == $course->association->university->id || Auth::user()->role === 'moderator' && Auth::user()->association->id == $course->association_id )
-                    <a class="button is-primary" href="{{ route( 'exams.create' ) }}">Lägg till tenta</a>
-                @endif
-            @else
-                <h3 class="subtitle">Inga tentor har lagts till på den här kursen, vill du lägga till en?</h3>
-                @auth
-                    @if( Auth::user()->role === 'super' || Auth::user()->role === 'admin' && Auth::user()->association->university->id == $course->association->university->id || Auth::user()->role === 'moderator' && Auth::user()->association->id == $course->association_id )
-                        <a class="button is-primary" href="{{ route( 'exams.create' ) }}">Lägg till tenta</a>
-                    @endif
-                @endauth
+                </div>
             @endif
+            <a class="button is-primary" href="{{ route( 'exams.create' ) }}">
+                Lägg till tenta
+            </a>
             @auth
                 @if( Auth::user()->role === 'super' || Auth::user()->role === 'admin' && Auth::user()->association->university->id == $course->association->university->id || Auth::user()->role === 'moderator' && Auth::user()->association->id == $course->association_id )
                     <hr>
