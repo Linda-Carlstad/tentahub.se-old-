c031f77142d8bb06c39022f08e92db8f7affc247
Felix Wetell <felix.wetell@gmail.com>
2019-12-02 18:11:44 +0100

    Improves UI #32 #12

--- resources/views/exams/index.blade.php
+++ resources/views/exams/index.blade.php
@@ -2,20 +2,34 @@
 @section( 'title', 'Tentor' )
 @section('content')
 
-    <section class="about section columns level">
-        <ul>
-            @foreach( $exams as $exam )
-                <li>
-                    <a href="{{ route( 'exams.show', $exam->id ) }}">
-                        {{ $exam->name }}
-                    </a>
-                    -
-                    <a href="{{ route( 'exams.download', $exam->id ) }}">
-                        Ladda ner
-                    </a>
-                </li>
-            @endforeach
-        </ul>
+    <section class="section">
+        <div class="columns level">
+            <div class="column is-half is-widescreen">
+                <h1 class="title">Tentor</h1>
+                <hr>
+                @if( $exams->isEmpty() )
+                    <p>Inga tentor att visa. Lägg till en!</p>
+                    <br>
+                @else
+                    <div class="list is-hoverable">
+                        @foreach( $exams as $exam )
+                            <p class="list-item">
+                                <a href="{{ route( 'exams.show', $exam->id ) }}">
+                                    {{ $exam->name }}
+                                </a>
+                                -
+                                <a href="{{ route( 'exams.download', $exam->id ) }}">
+                                    Ladda ner
+                                </a>
+                            </p>
+                        @endforeach
+                    </div>
+                @endif
+                <a class="button is-primary" href="{{ route( 'exams.create' ) }}">
+                    Lägg till tenta
+                </a>
+            </div>
+        </div>
     </section>
 
 @endsection
