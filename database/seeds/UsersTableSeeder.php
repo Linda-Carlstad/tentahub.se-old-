529ebe6e1f181ab1812c2da52f7f56304e928f99
Andreas Hortlund <hortlund.andreas@gmail.com>
2019-12-18 20:03:01 +0100

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        /*
         DB::table('users')->insert([
            'name' => 'Linda Carlstad info',
            'email' => 'info@lindacarlstad.se',
@@ -23,13 +24,13 @@ class UsersTableSeeder extends Seeder
            'created_at' => now(),
            'updated_at' => now(),
         ]);
-
+*/
         DB::table('users')->insert([
            'name' => 'Linda Carlstad utbildning',
             'email' => 'utbildning@lindacarlstad.se',
             'password' => bcrypt('BytMigSnartLinda'),
             'valid' => true,
-            'association_id' => 1,
+            'association_id' => 102,
             'role' => 'super',
             'email_verified_at' => now(),
             'created_at' => now(),
