77ab33a1efb5c7b41f38f2dbc22ff5391aec08c9
Andreas Hortlund <hortlund.andreas@gmail.com>
2019-12-20 13:03:51 +0100

    Update welcome.blade.php

--- resources/views/general/welcome.blade.php
+++ resources/views/general/welcome.blade.php
@@ -14,9 +14,36 @@
                 <p class="">Här kan du ladda ner tentasvar och dela med dig anonymt av dina egna svar</p>
                 <hr class="">
                 <p>Nedan hittar du tillgängliga föreningar.</p>
+
+
+                <h4 class="text-center">Antal: {{ $total }}</h4>
+        <table class="table table-hover">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Namn</th>
+                    <th>Melodi</th>
+                    <th>#</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($associations as $association)
+                    <tr>
+                        <td>{{ $$association->name }}</td>
+                        <td>
+                            <a href="{{ url( 'song/' . $song->id . '/edit') }}" class="btn btn-link">
+                                Redigera
+                            </a>
+                        </td>
+                    </tr>
+
+                @endforeach
+            </tbody>
+        </table>
+                <!--
                 <a class="button is-primary is-medium" href="#">Linda Carlstad</a>
                 <a class="button is-primary is-medium" href="#" role="button">Mexika</a>
                 <a class="button is-primary is-medium" href="#" role="button">Seke</a>
+                -->
             </div>
         </div>
     </section>
