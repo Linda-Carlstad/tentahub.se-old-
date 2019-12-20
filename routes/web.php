c8552fdfb3e7503efb2725517995883ebf291702
Andreas Hortlund <hortlund.andreas@gmail.com>
2019-12-20 13:17:33 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get( '/', 'AssociationController@welcome' );
+Route::get( '/', 'AssociationController@welcome' )->name( 'dashboard' );
 /*
 Route::get( '/', 'ShowDashboard' )->name( 'dashboard' );
 */
