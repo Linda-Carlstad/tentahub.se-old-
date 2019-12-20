9c83353169635cebc8a717400023709d42e192cb
Andreas Hortlund <hortlund.andreas@gmail.com>
2019-12-20 12:59:47 +0100

    Update welcome.blade.php

--- resources/views/general/welcome.blade.php
+++ resources/views/general/welcome.blade.php
@@ -14,7 +14,7 @@
                 <p class="">Här kan du ladda ner tentasvar och dela med dig anonymt av dina egna svar</p>
                 <hr class="">
                 <p>Nedan hittar du tillgängliga föreningar.</p>
-                <a class="button is-primary is-medium" href="{{ route( 'linda' ) }}">Linda Carlstad</a>
+                <a class="button is-primary is-medium" href="#">Linda Carlstad</a>
                 <a class="button is-primary is-medium" href="#" role="button">Mexika</a>
                 <a class="button is-primary is-medium" href="#" role="button">Seke</a>
             </div>
