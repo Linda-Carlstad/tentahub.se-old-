f14e6386edccfcf57c80413575ffc65032e71cf4
Felix Wetell <felix.wetell@gmail.com>
2020-01-15 20:44:21 +0100

    Updates course handling

---
+++ app/Events/Courses/CourseCreated.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Events\Courses;
+
+use App\Course;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class CourseCreated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $course;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param Course $course
+     */
+    public function __construct( Course $course )
+    {
+        $this->course = $course;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
