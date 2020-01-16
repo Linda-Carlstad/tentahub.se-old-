619f5d66059cae8713faf61d04dd6366cc98b0e5
Felix Wetell <felix.wetell@gmail.com>
2020-01-17 00:13:18 +0100

    Updates flash message structure #34

--- resources/views/partials/layouts/flash-messages.blade.php
+++ resources/views/partials/layouts/flash-messages.blade.php
@@ -1,20 +1,58 @@
 @if( session()->has( 'success' ) )
     <div class="notification is-success">
         <button class="delete"></button>
-        {{ session()->get( 'success' ) }}
+        <div class="columns level">
+            <div class="column is-1 has-text-centered level-item">
+                <i class="far fa-check-circle fa-3x"></i>
+            </div>
+            <div class="column is-11 level-item">
+                <h3 class="title is-4">Lyckat!</h3>
+                {{ session()->get( 'success' ) }}
+            </div>
+        </div>
     </div>
 @endif
 
 @if( session()->has( 'error' ) )
     <div class="notification is-danger">
         <button class="delete"></button>
-        {{ session()->get( 'error' ) }}
+        <div class="columns level">
+            <div class="column is-1 has-text-centered level-item">
+                <i class="far fa-times-circle fa-3x"></i>
+            </div>
+            <div class="column is-11 level-item">
+                <h3 class="title is-4">Attans!</h3>
+                {{ session()->get( 'error' ) }}
+            </div>
+        </div>
     </div>
 @endif
 
 @if( session( 'resent' ) )
     <div class="notification is-info">
         <button class="delete"></button>
-        Ett nytt verifieringsmail har skickats till din e-postadress.
+        <div class="columns level">
+            <div class="column is-1 has-text-centered level-item">
+                <i class="far fa-envelope fa-3x"></i>
+            </div>
+            <div class="column is-11 level-item">
+                <h3 class="title is-4">Saker</h3>
+                Ett nytt verifieringsmail har skickats till din e-postadress.
+            </div>
+        </div>
     </div>
 @endif
+
+
+<div class="notification is-success">
+    <button class="delete"></button>
+    <div class="columns level">
+        <div class="column is-1 has-text-centered level-item">
+            <i class="far fa-check-circle fa-3x"></i>
+        </div>
+        <div class="column is-11 level-item">
+            <h3 class="title is-4">Lyckat!</h3>
+            Ett nytt verifieringsmail har skickats till din e-postadress
+        </div>
+    </div>
+</div>
