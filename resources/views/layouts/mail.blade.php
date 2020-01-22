c4155a015d7cec5ff2148a3e719d07f563c42520
Felix Wetell <felix.wetell@gmail.com>
2020-01-22 13:25:11 +0100

    Updates mail CSS #21

--- resources/views/layouts/mail.blade.php
+++ resources/views/layouts/mail.blade.php
@@ -44,15 +44,21 @@
                 src: url('https://fonts.googleapis.com/css?family=Comfortaa|Raleway');
             }
 
+            body {
+                background-color: var( --color-scheme-background );
+                margin: 0;
+                font-family: 'Comfortaa', 'Raleway', sans-serif !important;
+            }
+
             a
             {
-                color: #00C851;
+                color: #ff3f34;
                 text-decoration: none;
             }
             a:hover
             {
-                color: #ffbb33;
-                text-decoration: none;
+                color: #cd0b00;
+                text-decoration: underline;
             }
 
             h3
@@ -60,16 +66,9 @@
                 margin-bottom: 5px;
             }
 
-            hr
-            {
-                width: 70%;
-                margin: 0 auto;
-                color: #363839;
-            }
-
             body
             {
-                width: 70%;
+                width: 80%;
                 margin: 20px auto;
             }
 
@@ -89,7 +88,8 @@
 
             header
             {
-                background-color: #fff;
+                background: linear-gradient(170deg, #ff3f34, #f7b731);
+                color: #fff;
                 padding: 20px 0;
                 width: 100%;
                 text-align: center;
@@ -97,25 +97,29 @@
                 border-top-right-radius: 4px;
             }
 
-            header h1
-            {
-                color: #00C851;
-            }
-
             .main
             {
                 width: 100%;
-                padding: 40px 20px;
+                background-color: #fff;
+                color: #000;
+            }
+
+            .main .container {
+                padding: 30px 30px;
             }
 
             footer
             {
+                border-top: 10px solid var(--color-scheme-background-diff);
+                background-color: var(--color-scheme-background-alt);
+                color: var( --color-scheme-text-color );
                 padding: 20px 0;
                 border-bottom-left-radius: 4px;
                 border-bottom-right-radius: 4px;
                 width: 100%;
                 text-align: center;
             }
+
         </style>
     </head>
     <body>
@@ -128,6 +132,7 @@
             <div class="main">
                 <div class="container">
                     @yield( 'content' )
+                </div>
             </div>
 
             <footer>
