cb4093224193bd790947e5d453651de476b09070
Felix Wetell <felix.wetell@gmail.com>
2020-02-21 11:38:16 +0100

    Adds university id hidden input

--- resources/views/courses/edit.blade.php
+++ resources/views/courses/edit.blade.php
@@ -70,8 +70,10 @@
                             </div>
                         </div>
                     </div>
+                    <input type="hidden" name="university_id" value="{{ Auth::user()->university->id }}">
                 @else
                     <input type="hidden" name="association_id" value="{{ Auth::user()->association->id }}">
+                    <input type="hidden" name="university_id" value="{{ Auth::user()->university->id }}">
                 @endif
                 <div class="field">
                     <label for="url" class="label">Länk</label>
