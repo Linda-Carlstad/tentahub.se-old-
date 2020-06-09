a80bd9d3d5f377abac890ab8654a6bb376743d4f
Felix Wetell <felix.wetell@gmail.com>
2020-06-09 12:01:26 +0200

    Updates redirect after login #92

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/profil';
+    protected $redirectTo = '/dashboard';
 
     /**
      * Create a new controller instance.
