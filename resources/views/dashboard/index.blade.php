3e363d424bcd509a7ec36bd49cdbe44c13788e4e
Felix Wetell <felix.wetell@gmail.com>
2020-06-09 11:59:48 +0200

    Updates dashboard to force user to change password

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -13,12 +13,20 @@
     <hr>
     <section class="hero is-medium has-text-centered">
         <div class="hero-body">
-            <h1 class="title">Vad vill du göra idag?</h1>
-            <h2 class="subtitle">Välj ett alternativ nedan:</h2>
-            <div class="buttons is-centered">
-                <a href="{{ route( 'courses.index') }}" class="button is-primary">Redigera Kurser</a>
-                <a href="{{ route( 'exams.index') }}" class="button is-primary">Redigera Tentor</a>
-            </div>
+            @if( Auth::user()->valid )
+                <h1 class="title">Vad vill du göra idag?</h1>
+                <h2 class="subtitle">Välj ett alternativ nedan:</h2>
+                <div class="buttons is-centered">
+                    <a href="{{ route( 'courses.index') }}" class="button is-primary">Redigera Kurser</a>
+                    <a href="{{ route( 'exams.index') }}" class="button is-primary">Redigera Tentor</a>
+                </div>
+            @else
+                <h1 class="title">Byt lösenord</h1>
+                <h2 class="subtitle">För att börja använda {{ config( 'app.name' ) }}, behöver ni byta lösenord.</h2>
+                <div class="buttons is-centered">
+                    <a href="{{ route( 'profile.settings') }}" class="button is-primary">Inställningar</a>
+                </div>
+            @endif
         </div>
     </section>
 
